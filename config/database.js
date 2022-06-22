9967d2e00c82e9d3526359773e26f5016b94a3e5
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-06-22 10:21:28 +0300

    Express and mongoose implement

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+// TODO change database name
+const dbName = wildlife
+const connectionString = "mongodb://localhost:27017/" + dbName;
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    
+        console.log(`Connected to ${dbName}`);
+    
+        mongoose.connection.on('error', (err) => console.log('Database error: ', err));
+    } catch (error) {
+        console.log(`Error connecting to ${dbName}: ${error}`);
+        process.exit(1);
+    }
+
+
+}
